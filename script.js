62713211c77e214fc90cb4dfa187906eb1dd7521
azzamdinabdillah <azzamdinabdillah123@gmail.com>
2025-06-07 10:23:01 +0700

    feat: add animation in hero section

--- script.js
+++ script.js
@@ -1,17 +1,57 @@
 import InfiniteMarquee from "vanilla-infinite-marquee";
+import gsap from "gsap";
+import TypeIt from "typeit";
 
-new InfiniteMarquee({
-  element: ".marquee-container",
-  speed: 25000,
-  smoothEdges: true,
-  direction: "right",
-  duplicateCount: 1,
-});
+document.addEventListener("DOMContentLoaded", function () {
+  new InfiniteMarquee({
+    element: ".marquee-container",
+    speed: 25000,
+    smoothEdges: true,
+    direction: "right",
+    duplicateCount: 1,
+  });
 
-new InfiniteMarquee({
-  element: ".marquee-container-2",
-  speed: 20000,
-  smoothEdges: true,
-  direction: "left",
-  duplicateCount: 0,
+  new InfiniteMarquee({
+    element: ".marquee-container-2",
+    speed: 20000,
+    smoothEdges: true,
+    direction: "left",
+    duplicateCount: 0,
+  });
+
+  // FLOATING TESTIMONIAL ANIMATION
+  gsap.set(".floating-animation", {
+    opacity: 0,
+    filter: "blur(5px)",
+  });
+
+  // MARQUEE FADEIN ANIMATION
+  gsap.set(".marquee-fadein", {
+    opacity: 0,
+    filter: "blur(5px)",
+  });
+
+  new TypeIt("#typewriter", {
+    speed: 70,
+    afterComplete: function () {
+      // FLOATING TESTIMONIAL ANIMATION
+      gsap.to(".floating-animation", {
+        opacity: 1,
+        filter: "blur(0px)",
+        stagger: {
+          each: 0.2,
+          from: "center",
+          grid: "auto",
+          ease: "power1",
+        },
+        duration: 1,
+      });
+
+      // MARQUEE FADEIN ANIMATION
+      gsap.to(".marquee-fadein", {
+        opacity: 1,
+        filter: "blur(0px)",
+      });
+    },
+  }).go();
 });
