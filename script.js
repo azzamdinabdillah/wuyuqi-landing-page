e3d43da99ee92c2e8b0baa95cb54892986bdb0d2
azzamdinabdillah <azzamdinabdillah123@gmail.com>
2025-06-11 14:39:29 +0700

    feat: add animation and fix some bug

--- script.js
+++ script.js
@@ -1,8 +1,86 @@
 import InfiniteMarquee from "vanilla-infinite-marquee";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import TypeIt from "typeit";
+import { CountUp } from "countup.js";
 
 document.addEventListener("DOMContentLoaded", function () {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // GENERAL SCRIPT
+
+  // FADEIN ANIMATION
+  gsap.fromTo(
+    ".fadein-item",
+    {
+      opacity: 0,
+      x: -100,
+    },
+    {
+      opacity: 1,
+      x: 0,
+      duration: 0.5,
+      ease: "power1.out",
+      scrollTrigger: {
+        trigger: ".fadein-item-wrapper",
+        start: "top 50%",
+      },
+      stagger: {
+        each: 0.2,
+      },
+    }
+  );
+
+  // END GENERAL SCRIPT
+
+  // ------------------------------------------------------------------------------------------------
+
+  // NAVBAR SCRIPT
+  // IF NAVBAR ON THE TOP OF THE PAGE
+  window.addEventListener("scroll", () => {
+    const navbar = document.querySelector("nav");
+    if (window.scrollY <= 0) {
+      navbar.classList.remove("bg-white");
+    } else {
+      navbar.classList.add("bg-white");
+    }
+  });
+
+  // SIDEBAR MOBILE
+  document.querySelector(".toggle-navbar").addEventListener("click", () => {
+    const navbarMobile = document.querySelector(".navbar-mobile");
+    navbarMobile.classList.toggle("show");
+    document.querySelector("body").classList.toggle("overflow-hidden");
+
+    // ANIMATION OPEN/CLOSE SIDEBAR MOBILE VERSION
+    gsap
+      .timeline({
+        defaults: {
+          duration: 0.5,
+        },
+      })
+      .set(".navbar-mobile .menu", {
+        width: 0,
+      })
+      .fromTo(
+        ".navbar-mobile",
+        {
+          opacity: 0,
+        },
+        {
+          opacity: 1,
+        }
+      )
+      .to(".navbar-mobile .menu", {
+        width: "70vw",
+        duration: 0.3,
+      });
+  });
+  // END NAVBAR SCRIPT
+
+  // ------------------------------------------------------------------------------------------------
+
+  // HERO SECTION SCRIPT
   // MARQUEE ANIMATION FOR HERO SECTION
   new InfiniteMarquee({
     element: ".marquee-container",
@@ -18,27 +96,6 @@ document.addEventListener("DOMContentLoaded", function () {
     speed: 20000,
     smoothEdges: true,
     direction: "left",
-    pauseOnHover: true,
-    duplicateCount: 0,
-  });
-
-  // MARQUEE ANIMATION FOR SUCCESS STORIES SECTION
-  new InfiniteMarquee({
-    element: ".marquee-container-stories",
-    speed: 15000,
-    smoothEdges: true,
-    direction: "left",
-    gap: "20px",
-    pauseOnHover: true,
-    duplicateCount: 0,
-  });
-
-  new InfiniteMarquee({
-    element: ".marquee-container-stories-reverse",
-    speed: 15000,
-    smoothEdges: true,
-    direction: "right",
-    gap: "20px",
     pauseOnHover: true,
     duplicateCount: 0,
   });
@@ -78,52 +135,53 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     },
   }).go();
+  // END HERO SECTION SCRIPT
 
-  // SIDEBAR MOBILE
-  document.querySelector(".toggle-navbar").addEventListener("click", () => {
-    const navbarMobile = document.querySelector(".navbar-mobile");
-    navbarMobile.classList.toggle("show");
-    document.querySelector("body").classList.toggle("overflow-hidden");
+  // ------------------------------------------------------------------------------------------------
 
-    // ANIMATION OPEN/CLOSE SIDEBAR MOBILE VERSION
-    gsap
-      .timeline({
-        defaults: {
-          duration: 0.5,
-        },
-      })
-      .set(".navbar-mobile .menu", {
-        width: 0,
-      })
-      .fromTo(
-        ".navbar-mobile",
-        {
-          opacity: 0,
-        },
-        {
-          opacity: 1,
-        }
-      )
-      .to(".navbar-mobile .menu", {
-        width: "70vw",
-        duration: 0.3,
-      });
-  });
-
+  // DEMO SECTION SCRIPT
   // VIDEO PLAY
-  document.querySelector(".video-play-button").addEventListener("click", () => {
-    document.querySelector("video").play();
+  document
+    .querySelector(".video-play-button")
+    .addEventListener("click", (e) => {
+      const modal = document.querySelector(".modal-demo-video");
+      modal.classList.toggle("open");
+
+      modal.addEventListener("click", (e) => {
+        e.stopPropagation();
+        modal.classList.remove("open");
+      });
+    });
+  // END DEMO SECTION SCRIPT
+
+  // ------------------------------------------------------------------------------------------------
+
+  // STORIES SECTION SCRIPT
+  // MARQUEE ANIMATION FOR SUCCESS STORIES SECTION
+  new InfiniteMarquee({
+    element: ".marquee-container-stories",
+    speed: 15000,
+    smoothEdges: true,
+    direction: "left",
+    gap: "20px",
+    pauseOnHover: true,
+    duplicateCount: 0,
   });
 
-  // IF NAVBAR ON THE TOP OF THE PAGE
-  window.addEventListener("scroll", () => {
-    const navbar = document.querySelector("nav");
-    if (window.scrollY <= 0) {
-      navbar.classList.remove("bg-white");
-    } else {
-      navbar.classList.add("bg-white");
-    }
+  new InfiniteMarquee({
+    element: ".marquee-container-stories-reverse",
+    speed: 15000,
+    smoothEdges: true,
+    direction: "right",
+    gap: "20px",
+    pauseOnHover: true,
+    duplicateCount: 0,
   });
+  // END STORIES SECTION SCRIPT
+
+  // ------------------------------------------------------------------------------------------------
+
+  // FAQ SECTION SCRIPT
 
   // ACCORDION FAQ SECTION
   const accordion = document.querySelectorAll(".accordion");
@@ -141,4 +199,18 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   });
+
+  // COUNTUP SECTION (UNDER FAQ SECTION)
+  const countUp = document.querySelectorAll(".countup span");
+  const valueCount = [590, 832, 95];
+  countUp.forEach((count, index) => {
+    new CountUp(count, valueCount[index], {
+      duration: 5,
+      enableScrollSpy: true,
+    }).start();
+  });
+
+  // END FAQ SECTION SCRIPT
+
+  // ------------------------------------------------------------------------------------------------
 });
